abc80e6bfdfdf4efbb47f3d690a7260873cfffc8
martinezjm1968 <121067795+martinezjm1968@users.noreply.github.com>
2023-05-12 16:07:55 -0300

    Entrega 3 V1

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { CartContext } from "../Context/CartContext";
 import { FaTrashAlt } from 'react-icons/fa';
 import { CotizacionDolar } from '../Context/CotizacionDolar';
+import './Cart.css';
 
 let precioPesos = 0;
 
@@ -12,29 +13,42 @@ export const Cart = () => {
 
 
     return (
-        <div className="container my-5">
-
-            <h2>Tu compra</h2>
-            <hr />
-
-            {
-                cart.map((item) => (
-                    <div className="card-body text-black">
-                        <div key={item.id}>
-                            <h4>{item.title}</h4>
-                            <img style={{ width: '80px', height: '80px' }} src={item.imageSource} />
-                            <p>Cantidad: {item.cantidad} unidades</p>
-                            <p>Subotal: ${(item.cantidad * dolar.oficial.value_sell * item.precio).toLocaleString()}</p>
-                            <button onClick={() => removeItem(item.id)} className="btn btn-danger"><FaTrashAlt /></button>
-                            <hr />
-                        </div>
+        <div className="contenedor_cart">
+            <div class="grid-container">
+                <div class="left-column">
+                    <div class="row">
+                        <h2>Tu compra</h2>
+                        <hr />
+                        {
+                            cart.map((item) => (
+                                <div className="card-body text-black">
+                                    <div key={item.id}>
+                                        <h4>{item.title}</h4>
+                                        <img style={{ width: '80px', height: '80px' }} src={item.imageSource} />
+                                        <p>Cantidad: {item.cantidad} unidades</p>
+                                        <p>Subotal: ${(item.cantidad * dolar.oficial.value_sell * item.precio).toLocaleString()}</p>
+                                        <button onClick={() => removeItem(item.id)} className="btn btn-danger"><FaTrashAlt /></button>
+                                        <hr />
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        
                     </div>
-                ))
-            }
-            <div>
-                <h4>TOTAL: ${totalCompra().toLocaleString()}</h4>
-                <hr />
-                <button onClick={emptyCart} className="btn btn-danger">Vaciar carrito</button>
+                </div>
+                <div class="right-column">
+                    <div class="box">Checkout</div>
+                    <br/>
+                    <button className="btn btn-info align-items-center">Costos de envío</button>
+                    <br/>
+                    <div>
+                            <br/>
+                            <h4>TOTAL: ${totalCompra().toLocaleString()}</h4>
+                            <hr />
+                            <button onClick={emptyCart} className="btn btn-danger">Vaciar carrito</button>
+                            <button onClick={emptyCart} className="btn btn-success">Finalizar Compra</button>
+                        </div>
+                </div>
             </div>
         </div>
     )
