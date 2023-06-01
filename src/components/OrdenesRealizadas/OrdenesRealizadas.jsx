885bf7815ee8a5d5f98175bf17e7f1be8f49f4c9
martinezjm1968 <121067795+martinezjm1968@users.noreply.github.com>
2023-06-01 12:06:49 -0300

    Entrega 3 V9.1

--- src/components/OrdenesRealizadas/OrdenesRealizadas.jsx
+++ src/components/OrdenesRealizadas/OrdenesRealizadas.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useContext } from "react";
-import { collection, query, where, getDocs, orderBy} from "firebase/firestore"
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore"
 import "firebase/firestore";
 import { db } from "../Firebase/Config.js"
 import { AuthContext } from "../Context/AuthContext";
@@ -12,7 +12,7 @@ export const OrdenesRealizadas = () => {
     const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(true)
     const { dolar } = useContext(CotizacionDolar)
-    
+
 
     // Obtén el usuario logeado y su dirección de correo electrónico
     //const { user } = useContext(AuthContext)
@@ -21,14 +21,14 @@ export const OrdenesRealizadas = () => {
 
     useEffect(() => {
         setLoading(true)
-        
+
         // 1.- Armar una referencia (sync)
         const ordenes = collection(db, "orders")
         const q = userEmail
-            ? query(ordenes, where("client.email", "==", userEmail),  orderBy("client.fecha"))
+            ? query(ordenes, where("client.email", "==", userEmail), orderBy("client.fecha"))
             : ordenes
         // 2.- Consumir esa referencia (async)
-        
+
         getDocs(q)
             .then((res) => {
                 const docs = res.docs.map((doc) => {
@@ -38,7 +38,7 @@ export const OrdenesRealizadas = () => {
                     }
                 })
 
-                
+
                 setOrders(docs)
 
             })
@@ -49,41 +49,60 @@ export const OrdenesRealizadas = () => {
     return (
 
         <div className="contenedor_cart">
-            <div className="grid-container">
-                <div className="left-column">
-                    <h4>Órdenes del usuario: {userEmail}</h4>
-                    {loading
-                        ? <Loader />
-                        : (
-                            <ul>
-                                {orders.map((order) => (
 
-                                    <div className="row">
-                                        <p>Nombre: {order.client.nombre}</p>
-                                        <p>Direccion: {order.client.direccion}</p>
-                                        <p>CUIT: {order.client.cuit}</p>
-                                        <p>Fecha de compra: {order.client.fecha.toDate().toLocaleDateString()}</p>
-                                        <p>Total: ${(order.total).toLocaleString()}</p>
+            <h4>Órdenes del usuario: {userEmail}</h4>
+            <div>
+
+
+                {loading
+                    ? <Loader />
+                    : (
+                        <ul>
+                            {orders.map((order) => (
+                                <div className="grid-container">
+                                    <div className="left-column">
                                         <div>
-                                            <ul>
-                                                <div className="right-column">
-                                                    {order.items.map((item) => (
-                                                        <li key={item.id}>
-                                                            <p> Producto: {item.nombre} </p>
-                                                            <p> Cantidad: {item.cantidad} </p>
-                                                            <p> Precio: u${item.precio}</p>
-                                                            <p> Precio u$s Actual: ${(item.precio * dolar.oficial.value_sell).toLocaleString()}</p>
-                                                        </li>
-                                                    ))}
+
+
+                                            <div className="row">
+                                                <div>
+                                                    <div className="card-body text-black">
+                                                        <p>Nombre: {order.client.nombre}</p>
+                                                        <p>Direccion: {order.client.direccion}</p>
+                                                        <p>CUIT: {order.client.cuit}</p>
+                                                        <p>Fecha de compra: {order.client.fecha.toDate().toLocaleDateString()}</p>
+                                                        <p>Total: ${(order.total).toLocaleString()}</p>
+                                                        <hr />
+                                                    </div>
                                                 </div>
-                                            </ul>
+                                            </div>
                                         </div>
-                                        <hr />
+                                        <div>
+
+                                            <div className="right-column">
+                                                <div className="row">
+                                                    <div className="card-body text-white bg-secondary">
+                                                        <ul >
+                                                            {order.items.map((item) => (
+                                                                <li key={item.id} >
+                                                                    <p> Producto: {item.nombre} </p>
+                                                                    <p> Cantidad: {item.cantidad} </p>
+                                                                    <p> Precio: u${item.precio}</p>
+                                                                    <p> Precio u$s Actual: ${(item.precio * dolar.oficial.value_sell).toLocaleString()}</p>
+                                                                </li>
+                                                            ))}
+                                                            
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
-                                ))}
-                            </ul>)
-                    }
-                </div>
+                                </div>
+                            ))}
+                        </ul>)
+                }
             </div>
         </div>
     );
